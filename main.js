503bc46dd628988768bb6d8ce23a69405fe75693
LXtutui <113383859+LXtutui@users.noreply.github.com>
2023-06-26 16:12:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,9 +13,23 @@ function setup(){
   canvas.center();
   video=createCapture(VIDEO);
   video.hide();
-  //poseNet=ml5.poseNet(video, modelLoaded);
-  //poseNet.on("pose", gotPoses);
+  poseNet=ml5.poseNet(video, modelLoaded);
+  poseNet.on("pose", gotPoses);
 }
 function takeSnapshot(){
   save("foto.jpg");
+}
+function modelLoaded(){
+  console.log("poseNet foi inicializado");
+}
+function gotPoses(results){
+  if (results.length>0){
+    console.log(results);
+    X=results[0].pose.nose.x-15;
+    Y=results[0].pose.nose.y-15;
+  }
+}
+function draw(){
+  image(video, 0, 0, 300, 300);
+  image(nariz, X, Y, 30, 30);
 }
